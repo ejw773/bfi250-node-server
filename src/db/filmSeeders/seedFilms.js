cd661957b20b23f57e6c1cda334f628d2667b90a
Elijah Wilcott <ejw773@gmail.com>
2021-11-27 08:36:18 -0500

    modifying seeder script

--- src/db/filmSeeders/seedFilms.js
+++ src/db/filmSeeders/seedFilms.js
@@ -4,16 +4,18 @@ require('../mongoose')
 const colors = require('colors')
 const Film = require('../../models/film')
 
-const bfi1952 = require('./1952')
-const bfi1962 = require('./1962')
-const bfi1972 = require('./1972')
-const bfi1982 = require('./1982')
-const bfi1992 = require('./1992')
-const bfi2002 = require('./2002')
-const bfi2012 = require('./2012')
+const films = require('./filmData')
+// const bfi1962 = require('./1962')
+// const bfi1972 = require('./1972')
+// const bfi1982 = require('./1982')
+// const bfi1992 = require('./1992')
+// const bfi2002 = require('./2002')
+// const bfi2012 = require('./2012')
 
 // Modify 'addTheseFilms' according to what you want added to the database
-const addTheseFilms = bfi1952.concat(bfi1962, bfi1972, bfi1982, bfi1992, bfi2002, bfi2012)
+// const addTheseFilms = bfi1952.concat(bfi1962, bfi1972, bfi1982, bfi1992, bfi2002, bfi2012)
+
+const addTheseFilms = bfi1952
 
 for (let i = 0; i < addTheseFilms.length; i++) {
     const film = new Film(addTheseFilms[i]);
