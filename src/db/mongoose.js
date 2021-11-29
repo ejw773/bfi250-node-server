5dca5513746305b212054683734d5de979227832
Elijah Wilcott <ejw773@gmail.com>
2021-11-29 00:30:43 -0500

    corrects database string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-const connectionString = process.env.MONGODB_URL + '/' + process.env.DB_NAME
-
-mongoose.connect(connectionString, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true
 })
