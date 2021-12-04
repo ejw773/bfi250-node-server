18d868a3726188a50cc34cbe08ac21ddb0d76b33
Elijah Wilcott <ejw773@gmail.com>
2021-12-03 20:26:47 -0500

    adds logger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 require('./db/mongoose')
 const express = require('express')
+const morgan = require('morgan')
+const fs = require('fs')
+const path = require('path')
 const cors = require('cors')
 
 const userRouter = require('./routers/user')
@@ -14,6 +17,9 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 app.use(cors())
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/endpoints.html');
